dbc490b300fe3e4a740a6f8b94e988c4f2edd9a0
jlbuild <juliabuildbot@gmail.com>
2021-12-04 09:55:05 +0000

    Ipopt_jll build 300.1400.400+0

--- src/wrappers/x86_64-apple-darwin-libgfortran3.jl
+++ src/wrappers/x86_64-apple-darwin-libgfortran3.jl
@@ -6,16 +6,11 @@ using MUMPS_seq_jll
 using OpenBLAS32_jll
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("Ipopt")
-JLLWrappers.@declare_executable_product(amplexe)
 JLLWrappers.@declare_library_product(libipopt, "@rpath/libipopt.3.dylib")
 JLLWrappers.@declare_library_product(libipoptamplinterface, "@rpath/libipoptamplinterface.3.dylib")
+JLLWrappers.@declare_executable_product(amplexe)
 function __init__()
     JLLWrappers.@generate_init_header(ASL_jll, MUMPS_seq_jll, OpenBLAS32_jll, CompilerSupportLibraries_jll)
-    JLLWrappers.@init_executable_product(
-        amplexe,
-        "bin/ipopt",
-    )
-
     JLLWrappers.@init_library_product(
         libipopt,
         "lib/libipopt.3.dylib",
@@ -26,6 +21,11 @@ function __init__()
         libipoptamplinterface,
         "lib/libipoptamplinterface.3.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        amplexe,
+        "bin/ipopt",
     )
 
     JLLWrappers.@generate_init_footer()
--- src/wrappers/x86_64-w64-mingw32-libgfortran3-cxx11.jl
+++ src/wrappers/x86_64-w64-mingw32-libgfortran3-cxx11.jl
@@ -6,16 +6,11 @@ using MUMPS_seq_jll
 using OpenBLAS32_jll
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("Ipopt")
-JLLWrappers.@declare_executable_product(amplexe)
 JLLWrappers.@declare_library_product(libipopt, "libipopt-3.dll")
 JLLWrappers.@declare_library_product(libipoptamplinterface, "libipoptamplinterface-3.dll")
+JLLWrappers.@declare_executable_product(amplexe)
 function __init__()
     JLLWrappers.@generate_init_header(ASL_jll, MUMPS_seq_jll, OpenBLAS32_jll, CompilerSupportLibraries_jll)
-    JLLWrappers.@init_executable_product(
-        amplexe,
-        "bin\\ipopt.exe",
-    )
-
     JLLWrappers.@init_library_product(
         libipopt,
         "bin\\libipopt-3.dll",
@@ -26,6 +21,11 @@ function __init__()
         libipoptamplinterface,
         "bin\\libipoptamplinterface-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        amplexe,
+        "bin\\ipopt.exe",
     )
 
     JLLWrappers.@generate_init_footer()
